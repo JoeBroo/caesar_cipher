b65a185fa23fac94cfa2ac284b4215d63762806b
JoeBroo <jdoe40630@gmail.com>
2023-09-05 23:26:12 -0400

    Refactor code

--- app.rb
+++ app.rb
@@ -1,24 +1,49 @@
-def test
-    puts "Welcome to the Caesar Cipher Program!
-    "
 
-    puts "Choose a word:"
-    clear_text = gets.chomp
-    puts "You chose #{clear_text}"
 
-    puts "Choose a number:"
-    number = gets.chomp
-    "You chose #{number}"
 
-    if clear_text == "cat"
-        cipher_text = "dbu
-        "
-        puts "Your encrypted wcord is: #{cipher_text}"
-        
-    else puts 'Choose the word "cat" and number "1".
-        '
-        test
-    end
+def caesar_cipher
+
+ #Create starting arrays
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+clear_text = []
+
+cipher_text = []
+
+#Get a phrase
+puts "Enter a phrase"
+
+p clear_text
+
+phrase = gets.chomp
+
+puts "You chose '#{phrase}'"
+
+#add phrase characters to clear_text array
+clear_text.push(phrase.chars)
+
+#flatten to turn nested array into regular array
+clear_text = clear_text.flatten
+p clear_text
+
+#Get a shift number
+puts "Choose a shift number"
+
+#Turn shift number from string into integer 
+shift_num = gets.chomp.to_i
+puts "You chose #{shift_num}"
+
+#Checks if shift number is zero and returns 'a' if it is
+ if shift_num == 0
+    puts alphabet[0]
+
+#Otherwise it returns the corresponding letter of shift num
+ else 
+    puts alphabet[shift_num]
+ end
+
 end
 
-test
+caesar_cipher
