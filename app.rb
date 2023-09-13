32467dfaaf34b6d0ed96a7fa6940cd6481732f7f
JoeBroo <jdoe40630@gmail.com>
2023-09-12 22:16:26 -0400

    Check if ordinal number is in alphabet

--- app.rb
+++ app.rb
@@ -1,29 +1,56 @@
-
-
-
 def caesar_cipher(string, shift_number)
 
-    #Get each character of string
+    encrypted_phrase = ""
+
+    
     string_chars = string.split('')
 
     #Change each character of string into ASCII
     ordinals = string_chars.map {|char| char.ord }
 
     #Add shift_number to ASCII of character
-    added = ordinals.map {|ord| ord + shift_number }
+    added = ordinals.map do |ord|
 
-    #Change ASCII code to characters
-    ordinals_characters = added.map {|num| num.chr}
+        #Check if ordinal is an uppercase letter
+        if ord >= 65 && ord <= 90
 
-    p string_chars
-    p ordinals
-    p ordinals_characters
+            added = ord += shift_number
+            
+             
+            if added > 90
 
-    puts "#{string}'s characters are: #{string_chars} and
-    your number is #{shift_number}"
+                added = added - 90 + 65 - 1
+
+                #Change ordinal into letter and add it to encrypted_phrase
+                encrypted_phrase.concat(added.chr)
+
+            else
+                encrypted_phrase.concat(added.chr)
+            end
+
+        #Check if ordinal is lowercase letter 
+        elsif ord >= 97 && ord <= 122
+
+            added = ord += shift_number
+            
+            if added > 122
+                added = added - 122 + 97 - 1
+
+                encrypted_phrase.concat(added.chr)
+
+            else
+                encrypted_phrase.concat(added.chr)
+            end
+
+        else
+
+            encrypted_phrase.concat(ord.chr)
+        end
+
+    end
+
+    puts "Your encrypted phrase is: #{encrypted_phrase}"
 
 end
 
 caesar_cipher("What a string!", 5)
-
-
